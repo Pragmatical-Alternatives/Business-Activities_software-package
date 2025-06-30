09d850b1dcd411b57bbd941c2c6e03da39787570
ietzel <67884277+ietzel@users.noreply.github.com>
2025-06-30 11:58:06 -0400

    Update a.cs

--- a.cs
+++ a.cs
@@ -4,6 +4,9 @@ namespace HelloWorldApp {
     class Geeks {    
         static void Main(string[] args) {            
             Console.WriteLine("HR Chatbot"); 
+            Console.WriteLine("Any obstructions?"); 
+            int userName = Console.ReadLine();
+            Console.WriteLine("No is my favorite emotion."); 
             Console.ReadKey(); 
         } 
     } 
